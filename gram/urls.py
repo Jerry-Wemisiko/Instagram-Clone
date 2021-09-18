8cfb08edfbd65f9be3995a71913683950dedbd91
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-18 14:42:15 +0300

    migrations

--- gram/urls.py
+++ gram/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns =  [
     path('',views.homepage, name='homepage'),
     path('su',views.signUp,name = 'signin'),
     path('signUp', views.signIn, name='signUp'),
